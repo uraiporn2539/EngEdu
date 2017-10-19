16c3173a50ea20dd72bbe82f5591efeafcbb5bdc
________ Meawmeaw <caretie_caretie@hotmail.com>
2017-10-19 22:24:04 +0700

    first year semester 2

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -11,6 +11,7 @@ namespace EngEdu
         static void Main(string[] args)
         {
             Console.WriteLine("Year 255x semester 1,I am Engedu freshman.");
+            Console.WriteLine("Year 255x semester 2,I learn to program in C sharp.");
         }
     }
 }
